4082de740ac79de9847212c9fe2f6f9c7ad0dbef
RaihanK-ac <raihankobirgithubac.bd@gmail.com>
2025-10-14 20:41:45 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+import { ScrollTrigger,SplitText } from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+
+
+export default function Home() {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello This An Animated Landing Page</h1>
+    </div>
+  );
+}
