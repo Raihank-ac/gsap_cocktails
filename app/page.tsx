ea7ff2f02147a92441f52a46731003ede594d799
RaihanK-ac <raihankobirgithubac.bd@gmail.com>
2025-10-15 14:52:46 +0600

    Implement Cocktails Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import gsap from "gsap";
@@ -14,7 +15,7 @@ export default function Home() {
 <main>
   <Navbar/>
   <Hero/>
-  
+  <Cocktails/>
 </main>
   );
 }
