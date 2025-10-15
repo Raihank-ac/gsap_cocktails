11fce03810f61bebff4f38405130cf30cee0f14b
RaihanK-ac <raihankobirgithubac.bd@gmail.com>
2025-10-15 18:07:40 +0600

    implement The Art Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Art from "@/components/Art";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
@@ -18,6 +19,7 @@ export default function Home() {
   <Hero/>
   <Cocktails/>
   <About/>
+  <Art/>
 </main>
   );
 }
